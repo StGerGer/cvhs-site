3eb599df677a73e13e462bb8338e734682a5a645
Nate St. George <nateestes223@gmail.com>
2016-11-11 10:41:02 -0700

    minor changes yay

--- js/activities.js
+++ js/activities.js
@@ -11,12 +11,14 @@ app.controller("activitiesCtrl", function($scope) {
             club: 'club',
             sponsor: 'sponsor',
             email: 'email@email.co.uk.com.net',
+            location: 'everywhere',
             time: 'time o\' clock'
         },
         {
             club: 'club 2',
             sponsor: 'sponsor 2',
             email: 'jesus@butts.org',
+            location: 'las vegas',
             time: 'nah'
         }
     ];
