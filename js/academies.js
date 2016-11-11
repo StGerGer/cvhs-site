b17bca70c967c4d67d29f8b1d18b7bd9c04beb28
Nate St. George <nateestes223@gmail.com>
2016-11-11 10:54:18 -0700

    <div class='really-small'>have my babies</div>

--- js/academies.js
+++ js/academies.js
@@ -37,20 +37,20 @@ app.controller("academiesCtrl", function($scope) {
 
 pageContentObjects = [
     {name: 'bhs', html: `
-        <div class="bhs academyHeader noselect">BHS</div> <br>
+        <div class="bhs academyHeader no-select">BHS</div> <br>
         The Biotechnology/Health Sciences Academy is designed to help students become: problem solvers, critical thinkers, self-directed learners, excellent communicators and effective team members. Assuring their success in a rigorous curriculum designed to prepare students for work or future study. The BHS Academy emphasizes team-based learning focused on relevant situations. Students gain skills necessary to perform duties in many biotechnical and health science settings. <br> <br>
         The Biotechnology/Health Science Academy was created to meet interest demands of students and the labor needs of the community. Projections from the U.S. Department of Labor indicate that health care careers will experience the greatest growth in the next ten years. Biotechnology will also experience a high demand for qualified professionals as this science expands. <br> <br>
         Biotechnology Pathway This pathway provides three in-depth courses in biotechnology or the science of DNA. Students at the sophomore level will begin their study of Biotechnology I and progress to Biotechnology III in their senior year. From this point, they will enter internship opportunities with cooperating nearby institutions. Students learn the basics of gel electrophoresis, PCR, micro pipetting, solution preparation, aseptic technique and other procedures used in biotechnology. Students learn patterns of Inheritance, Protein Synthesis, epigenesis, mutations, the molecular biology of cancer, knowledge and use of adult and embryonic stem cells, biomarkers, evolutionary genomics and other knowledge basic to the understanding of genomes. Students also explore the ethics of using stem cells, gene testing, gene therapy, etc., using models for ethical decisions developed by the medical community. <br> <br>
 
     `},
     {name: 'stem', html: `
-        <div class="stem academyHeader noselect">STEM</div> <br>
+        <div class="stem academyHeader no-select">STEM</div> <br>
         The Science, Technology, Math & Engineering Academy offers innovative courses in science, technology, engineering, and mathematics with a supportive staff helping all students achieve their potential while acknowledging each individual’s learning style. <br> <br>
         Envisioning education as a partnership among students, parents, staff, and community, the STEM Academy offers a program with diversified educational opportunities including engaging classroom instruction, field trips, guest speakers, and internship opportunities. <br> <br>
         Students in the STEM Academy can take advantage of Castle View’s innovative schedule to study science, technology, math, and engineering concepts at a deep level with teachers dedicated to help them gain and apply skills leading to a variety of higher education and career opportunities in engineering, computer science, research, and related fields.
     `},
     {name: 'vpa', html: `
-        <div class="vpa academyHeader noselect">VPA</div> <br>
+        <div class="vpa academyHeader no-select">VPA</div> <br>
         CVHS students in the Visual and Performing Arts Academy receive rigorous preparation in core subject areas with instruction based on awareness of these students’ primary learning styles – visual, auditory and kinesthetic. <br> <br>
         Study of the arts encourages students to develop the tools of thinking itself: careful observation of the world, mental representation of what is observed or imagined, abstraction from complexity, symbolic representation and qualitative judgment. <br> <br>
         Study of the arts helps students connect intellectual academic accomplishments with creative expression and experience a truly balanced education by linking cognitive growth to social and emotional development focused on skills and interests. <br> <br>
@@ -58,7 +58,7 @@ pageContentObjects = [
         Study of the arts provides students a supportive environment that celebrates personal talents with opportunities to excel in areas of giftedness such as art, dance, drama, instrumental music and voice.
     `},
     {name: 'lgc', html: `
-        <div class="lgc academyHeader noselect">LGC</div> <br>
+        <div class="lgc academyHeader no-select">LGC</div> <br>
         In the Leadership, Global Studies, and Communication Academy, students are prepared to enter the global economy with the necessary skills to understand the world around them, communicate effectively, and take action in local, national, and global issues. Benefiting from a wide range of community and professional relationships, LGC students will gain real world experience working alongside professionals including journalists, business leaders, professors, lawyers, and politicians. <br> <br>
         All students in the LGC Academy will receive a solid fundamental education in core areas such as English, Math, Social Studies, the sciences and the arts. From there, students may choose to follow a pathway offering experiences in Leadership, Law and Government, Communication, or Global Studies. <br> <br>
         Leadership Pathway - Discover and hone your potential. Leadership is a theme that will be woven throughout the academy. Students will explore what it means to lead, and will be given ample opportunities to exercise their skills. Specific courses will be offered in each pathway providing opportunities to formally study leadership and to participate in selected projects. Leadership is action, not position, and the LGC Academy will prepare students to take action in a variety of ways. Students will be equipped and encouraged to lead on campus as peer tutors and counselors or members of student government. Others may champion a charitable cause, captain an athletic team, or lead an activity or club. Leadership skills learned and practiced in academy activities may be applied anywhere in the school and community. Learn to lead then see where your vision takes you! <br> <br>
